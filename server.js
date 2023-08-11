2f603a45a6a43941ad4acee43442295e1bae74ec
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-08-11 21:19:25 +0300

    Update
    add env
    add lib dotenv
    config server.js
    checked on port(300)  and host(render)

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-// const DB_HOST =
-//   "mongodb+srv://Vladyslav:nKkTMtrp8QSGU9Wr@cluster0.axzivqu.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
-
-// console.log(process.env.DB_HOST);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connect success");
   })
   .catch((error) => console.log(error.message));
-
-// nKkTMtrp8QSGU9Wr
