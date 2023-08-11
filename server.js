2b9ffc2b2559ad120f3790696e6657d4c5a9c7d2
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-08-11 20:55:15 +0300

    update

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Vladyslav:nKkTMtrp8QSGU9Wr@cluster0.axzivqu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://Vladyslav:nKkTMtrp8QSGU9Wr@cluster0.axzivqu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
+
+// console.log(process.env.DB_HOST);
 
 mongoose
   .connect(DB_HOST)
