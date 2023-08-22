822644f71c210244c3b4ea966eeba3d8f4a46cb7
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-08-23 00:02:59 +0300

    update

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -3,7 +3,7 @@ const { HttpError } = require("../helpers");
 const { SECRET_KEY } = process.env;
 const User = require("../models/user");
 
-const authenticate = (req, res, next) => {
+const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
@@ -13,11 +13,13 @@ const authenticate = (req, res, next) => {
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
-    const user = User.findById(id);
+    const user = await User.findById(id);
 
     if (!user) {
       next(HttpError(401));
     }
+
+    req.user = user;
 
     next();
   } catch {
